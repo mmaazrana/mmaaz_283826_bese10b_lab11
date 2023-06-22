0f3ca9713fc913c7ad495d0c0ef57135aa2f886c
Maaz Rana <muhammadmaaz@Maazs-MacBook-Pro-2.local>
2023-06-23 04:40:18 +0500

    initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# mmaaz_283826_bese10b_lab11
+
+A new Flutter project.
---
+++ android/app/src/main/kotlin/com/example/mmaaz_283826_bese10b_lab11/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.mmaaz_283826_bese10b_lab11
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
